80b2d03ebfb1f77262f79271e5e204901897ee86
Honee52 <148609665+Honee52@users.noreply.github.com>
2023-10-22 22:16:46 +0300

    Update TD_BME280.h

--- src/TD_BME280.h
+++ src/TD_BME280.h
@@ -2,7 +2,7 @@
 /*!
  * ----------------------------------------------------------------------------
  * @file TD_BME280.h
- * I2C driver for Bosch BME20 sensor (humidy, temperature and pressure).
+ * I2C driver for Bosch BME20 sensor (humidity, temperature and pressure).
  * Written by Esko Honkanen for Technode Design (info@technode.fi).
  * You may use this library as it is or change it without limitations. 
  * Beerware license.
